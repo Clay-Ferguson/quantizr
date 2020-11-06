cd5ecfd670a796c80ee3fa96acf910121971b51a
Clay Ferguson <wclayf@gmail.com>
2020-11-05 21:58:46 -0600

    massive D3 graph improvements

--- src/main/resources/public/ts/widget/FullScreenGraphViewer.ts
+++ src/main/resources/public/ts/widget/FullScreenGraphViewer.ts
@@ -11,21 +11,16 @@ import { Constants as C } from "../Constants";
 // https://www.npmjs.com/package/d3
 // https://d3js.org/
 
-// todo-0: our dragging/panning doesn't work but this is a simiar example that DOES.
-// http://bl.ocks.org/FrissAnalytics/e9ce129a11dad7441eecacbbb6bad95c
-
-// http://bl.ocks.org/eyaler/10586116
-
-
 let S: Singletons;
 PubSub.sub(C.PUBSUB_SingletonsReady, (ctx: Singletons) => {
     S = ctx;
 });
 
 export class FullScreenGraphViewer extends Main {
-
     nodeId: string;
     simulation: any;
+    tooltip: any;
+    isDragging: boolean;
 
     constructor(appState: AppState) {
         super();
@@ -49,108 +44,176 @@ export class FullScreenGraphViewer extends Main {
     }
 
     domPreUpdateEvent = (): void => {
-        let _this = this;
         let state = this.getState();
         if (!state.data) return;
 
-        const root = d3.hierarchy(state.data);
-        const links = root.links();
-        const nodes = root.descendants();
+        let customForceDirectedTree = this.forceDirectedTree();
 
-        const zoom = d3.zoom()
-            .scaleExtent([1, 10])
-            .on("zoom", zoomed);
+        d3.select(".d3Graph")
+            .datum(state.data)
+            .call(customForceDirectedTree);
+    }
 
-        this.stopSim();
+    forceDirectedTree = () => {
+        let _this = this;
+        let margin = { top: 0, right: 0, bottom: 0, left: 0 };
+        let width = window.innerWidth;
+        let height = window.innerHeight;
 
-        this.simulation = d3.forceSimulation(nodes)
-            .force("link", d3.forceLink(links).id(d => d.id).distance(0).strength(1))
-            .force("charge", d3.forceManyBody().strength(-50))
-            .force("x", d3.forceX())
-            .force("y", d3.forceY());
+        function chart(selection) {
+            let data = selection.datum();
+            let chartWidth = width - margin.left - margin.right;
+            let chartHeight = height - margin.top - margin.bottom;
 
-        const svg = d3.select(".d3Graph")
-            .append("svg")
-            .attr("width", "100%")
-            .attr("viewBox", [-window.innerWidth / 2, -window.innerHeight / 2, window.innerWidth, window.innerHeight])
-            .style("pointer-events", "all");
+            let root = d3.hierarchy(data);
+            let links = root.links();
+            let nodes = root.descendants();
 
-        function zoomed(event) {
-            const { transform } = event;
-            svg.attr("transform", transform);
-            svg.attr("stroke-width", 1 / transform.k);
+            let simulation = d3.forceSimulation(nodes)
+                .force("link", d3.forceLink(links).id(d => d.id).distance(0).strength(1))
+                .force("charge", d3.forceManyBody().strength(-50))
+                .force("x", d3.forceX())
+                .force("y", d3.forceY());
+
+            // I think I have a div inside this div when it renders which would be redundant (todo-0)
+            _this.tooltip = selection
+                .append("div")
+                .attr("class", "tooltip")
+                .style("font-size", "14px")
+                .style("pointer-events", "none");
+
+            let mouseover = (event: any, d) => {
+                if (d.data.id.startsWith("/")) {
+                    _this.updateTooltip(d, event.pageX, event.pageY);
+                }
+                else {
+                    _this.showTooltip(d, event.pageX, event.pageY);
+                }
+            };
+
+            let mouseout = () => {
+                _this.tooltip.transition()
+                    .duration(300)
+                    .style("opacity", 0);
+            };
+
+            let drag = function (simulation) {
+                function dragstarted(event, d) {
+                    _this.isDragging = true;
+                    if (!event.active) simulation.alphaTarget(0.3).restart();
+                    d.fx = d.x;
+                    d.fy = d.y;
+                }
+
+                function dragged(event, d) {
+                    d.fx = event.x;
+                    d.fy = event.y;
+                }
+
+                function dragended(event, d) {
+                    if (!event.active) simulation.alphaTarget(0);
+                    d.fx = null;
+                    d.fy = null;
+                    _this.isDragging = false;
+                }
+
+                return d3.drag()
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                    .on("end", dragended);
+            };
+
+            let svg = selection
+                .selectAll("svg")
+                .data([data])
+                .enter()
+                .append("svg")
+                .attr("width", chartWidth)
+                .attr("height", chartHeight)
+                .style("cursor", "move")
+                .attr("viewBox", [-window.innerWidth / 2, -window.innerHeight / 2, window.innerWidth, window.innerHeight]);
+            svg = svg.merge(svg);
+            let g = svg.append("g");
+
+            let link = g.append("g")
+                .attr("stroke", "#999")
+                .attr("stroke-width", 1.5)
+                .attr("stroke-opacity", 0.6)
+                .selectAll("line") // PathShape (leave this comment, referenced below)
+                .data(links)
+                .join("line"); // PathShape (leave this comment, referenced below)
+
+            let node = g.append("g")
+                .attr("stroke-width", 1.5)
+                .style("cursor", "pointer")
+                .selectAll("circle")
+                .data(nodes)
+                .join("circle")
+
+                .attr("fill", d => {
+                    if (d.data.id === _this.nodeId) return "red";
+                    return d.data.highlight ? "green" : "black";
+                })
+                .attr("stroke", d => {
+                    return _this.getColorForLevel(d.data.level);
+                })
+                .attr("r", d => {
+                    if (d.data.id === _this.nodeId) return 5;
+                    return 3.5;
+                })
+
+                .on("mouseover", mouseover)
+                .on("mouseout", mouseout)
+
+                .on("click", function (event: any, d) {
+                    // todo-0: need to bring back this border color setting.
+                    // circle
+                    //     // .style("fill", "lightcoral")
+                    //     .style("stroke", "green");
+                    if (d.data.id) {
+                        window.open(S.util.getHostAndPort() + "/app?id=" + d.data.id, "_blank");
+                    }
+                })
+
+                .call(drag(simulation));
+
+            simulation.on("tick", () => {
+                // -------------------
+                // DO NOT DELETE (if PathShape (above) is 'path' use this)
+                // link.attr("d", function (d) {
+                //     let dx = d.target.x - d.source.x;
+                //     let dy = d.target.y - d.source.y;
+                //     let dr = Math.sqrt(dx * dx + dy * dy);
+                //     return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 1 0,1 " + d.target.x + "," + d.target.y;
+                // });
+                // node
+                //     .attr("cx", d => d.x)
+                //     .attr("cy", d => d.y);
+                // -------------------
+
+                // If PathShape (above) is 'line' do this
+                link
+                    .attr("x1", d => d.source.x)
+                    .attr("y1", d => d.source.y)
+                    .attr("x2", d => d.target.x)
+                    .attr("y2", d => d.target.y);
+
+                node
+                    .attr("cx", d => d.x)
+                    .attr("cy", d => d.y);
+            });
+
+            let zoomHandler = d3.zoom()
+                .on("zoom", zoomAction);
+
+            function zoomAction(event) {
+                const { transform } = event;
+                g.attr("stroke-width", 1 / transform.k);
+                g.attr("transform", transform);
+            }
+            zoomHandler(svg);
         }
-
-        const link = svg.append("g")
-            .attr("stroke", "#999")
-            .attr("stroke-opacity", 0.6)
-            .selectAll("line")
-            .data(links)
-            .join("line");
-
-        const node = svg.append("g")
-            // .attr("fill", "#fff")
-            // .attr("stroke", "#fff")
-            .attr("stroke-width", 1.5)
-            .style("cursor", "pointer")
-            .selectAll("circle")
-            .data(nodes)
-            .join("circle")
-            .attr("fill", d => {
-                if (d.data.id === _this.nodeId) return "red";
-                return d.data.highlight ? "green" : "black";
-            })
-            .attr("stroke", d => {
-                return _this.getColorForLevel(d.data.level);
-            })
-            .attr("r", d => {
-                if (d.data.id === _this.nodeId) return 5;
-                return 3.5;
-            })
-            .call(this.drag(this.simulation));
-
-        node.on("click", function () {
-            let circle = d3.select(this);
-            let node = circle.node();
-            let data: J.GraphNode = node.__data__.data;
-            circle
-                // .style("fill", "lightcoral")
-                .style("stroke", "green");
-
-            if (data.id) {
-                window.open(S.util.getHostAndPort() + "/app?id=" + data.id, "_blank");
-            }
-        });
-
-        node.on("mouseover", function () {
-            let circle = d3.select(this);
-            let node = circle.node();
-            let data: J.GraphNode = node.__data__.data;
-
-            let title = circle.select("title");
-            if (data.id.startsWith("/")) {
-                title.text("Loading...");
-                _this.updateText(data.id, title);
-            }
-        });
-
-        node.append("title").text(d => {
-            return d.data.name || d.data.id;
-        });
-
-        this.simulation.on("tick", () => {
-            link
-                .attr("x1", d => d.source.x)
-                .attr("y1", d => d.source.y)
-                .attr("x2", d => d.target.x)
-                .attr("y2", d => d.target.y);
-
-            node
-                .attr("cx", d => d.x)
-                .attr("cy", d => d.y);
-        });
-
-        svg.call(zoom);
+        return chart;
     }
 
     getColorForLevel(level: number): string {
@@ -184,20 +247,9 @@ export class FullScreenGraphViewer extends Main {
         }
     }
 
-    domRemoveEvent = () => {
-        this.stopSim();
-    }
-
-    stopSim = () => {
-        if (this.simulation) {
-            this.simulation.stop();
-            this.simulation = null;
-        }
-    }
-
-    updateText = (nodeId: string, title: any) => {
+    updateTooltip = (d: any, x: number, y: number) => {
         const res = S.util.ajax<J.RenderNodeRequest, J.RenderNodeResponse>("renderNode", {
-            nodeId,
+            nodeId: d.data.id,
             upLevel: false,
             siblingOffset: 0,
             renderParentIfLeaf: false,
@@ -212,33 +264,31 @@ export class FullScreenGraphViewer extends Main {
                     if (content.length > 100) {
                         content = content.substring(0, 100) + "...";
                     }
-                    title.text(content);
+                    d.data.name = content;
+                    this.showTooltip(d, x, y);
                 }
             });
     }
 
-    drag = simulation => {
+    showTooltip = (d: any, x: number, y: number) => {
+        this.tooltip.transition()
+            .duration(300)
+            .style("opacity", (d) => (!this.isDragging) ? 0.97 : 0);
 
-        function dragstarted(event, d) {
-            if (!event.active) simulation.alphaTarget(0.3).restart();
-            d.fx = d.x;
-            d.fy = d.y;
+        this.tooltip.html(() => {
+            return "<div class='alert alert-secondary'>" + d.data.name + "</div>";
+        }).style("left", (x + 15) + "px")
+            .style("top", (y - 50) + "px");
+    }
+
+    domRemoveEvent = () => {
+        this.stopSim();
+    }
+
+    stopSim = () => {
+        if (this.simulation) {
+            this.simulation.stop();
+            this.simulation = null;
         }
-
-        function dragged(event, d) {
-            d.fx = event.x;
-            d.fy = event.y;
-        }
-
-        function dragended(event, d) {
-            if (!event.active) simulation.alphaTarget(0);
-            d.fx = null;
-            d.fy = null;
-        }
-
-        return d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended);
     }
 }
