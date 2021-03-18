cdba687e46b3c0db4a65092cd7c24cbef8acf04d
Clay Ferguson <wclayf@gmail.com>
2021-03-17 20:09:58 -0500

    menu state bug fix

--- src/main/resources/public/ts/widget/Menu.ts
+++ src/main/resources/public/ts/widget/Menu.ts
@@ -32,7 +32,7 @@ export class Menu extends Div {
                     setTimeout(() => {
                         /* "aria-expanded" attribute won't have been updated yet when this onClick is called, so we have a delay
                         timer here to wait for it to get updated */
-                        let expanded = elm.target.getAttribute("aria-expanded") === "true";
+                        let expanded = document.getElementById("heading" + this.getId()).getAttribute("aria-expanded") === "true";
                         Menu.activeMenu = expanded ? this.name : null;
                         // console.log("Expand or collapse: " + this.name + " expan=" + expanded);
                     }, 500);
