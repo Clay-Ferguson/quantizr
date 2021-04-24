be1235d285f7ac0117f1fa5d0d7106a16608de02
Clay Ferguson <wclayf@gmail.com>
2021-04-24 15:33:09 -0500

    default to RSS headlinesOnly=false

--- src/main/resources/public/ts/AppState.ts
+++ src/main/resources/public/ts/AppState.ts
@@ -126,7 +126,7 @@ export class AppState {
 
     // Rss feeds (cached by a hash of the feedSrc property text as the key)
     feedCache = {};
-    rssHeadlinesOnly: boolean = true;
+    rssHeadlinesOnly: boolean = false;
 
     // Similar to feedCache but holds the current 'page' the user is on (1, 2, 3,...) based on the user
     // being able to click 'more...' button to page thru a feed.
