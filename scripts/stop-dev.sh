f9e26832faa13e5165035cf2494dd45f32bc3dd2
Clay Ferguson <wclayf@gmail.com>
2021-05-25 09:54:35 -0500

    script updates

--- scripts/stop-dev.sh
+++ scripts/stop-dev.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # set -x
 
+# force current dir to be this script
+script_file=$(realpath $0)
+script_folder="$(dirname "${script_file}")"
+cd ${script_folder}
+
 source ./setenv-dev.sh
 
  cd ${DEPLOY_TARGET}
