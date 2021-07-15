3fffe81f728323cf7e5f27729ed0a6caa4480750
Clay Ferguson <wclayf@gmail.com>
2021-07-15 12:26:51 -0500

    webpack syntax variation

--- src/main/resources/public/webpack.common.js
+++ src/main/resources/public/webpack.common.js
@@ -32,19 +32,22 @@ module.exports = {
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             {
                 test: /\.tsx?$/,
-                loader: "awesome-typescript-loader",
-
-                options: {
-                    // Use this to point to your tsconfig.json.
-                    configFileName: "./tsconfig." + env + ".json"
-                }
+                use: [{
+                    loader: "awesome-typescript-loader",
+                    options: {
+                        // Use this to point to your tsconfig.json.
+                        configFileName: "./tsconfig." + env + ".json"
+                    }
+                }]
             },
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             {
-                enforce: "pre",
                 test: /\.js$/,
-                loader: "source-map-loader",
+                enforce: "pre",
+                use: [{
+                    loader: "source-map-loader"
+                }],
                 exclude: [
                     // this package has problems with their sourcemaps
                     path.resolve(__dirname, "node_modules/chardet")
@@ -53,12 +56,18 @@ module.exports = {
 
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }]
             },
 
             {
                 test: /\.htm$/,
-                use: ["html-loader"]
+                use: [{
+                    loader: "html-loader"
+                }]
             }
         ]
     },
