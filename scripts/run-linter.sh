f08f81efa597c3530e640cf0c2f66e61bef85cd0
Clay Ferguson <wclayf@gmail.com>
2021-07-16 11:36:21 -0500

    linter note

--- scripts/run-linter.sh
+++ scripts/run-linter.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# NOTE: I'd love to get this move into webpack (done by webpack), but there's a million different
+# pages online about how to do this and every one of them uses a completely different technique
+# so for now I'm just leaving this as is, since it works fine.
+
 cd ${PRJROOT}/src/main/resources/public
-
-# ./node_modules/.bin/eslint --debug ./**/*.ts 
-
 ./node_modules/.bin/eslint . --ext .ts 
 
 # add this param to the end to auto-fix
