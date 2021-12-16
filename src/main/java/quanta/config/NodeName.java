08e0571fe0f450c7ccea07c7fd64f52337931bf6
Clay Ferguson <wclayf@gmail.com>
2021-12-16 15:51:16 -0600

    comment update

--- src/main/java/quanta/config/NodeName.java
+++ src/main/java/quanta/config/NodeName.java
@@ -1,11 +1,5 @@
 package quanta.config;
 
-/**
- * 
- * todo-2: some of these are used for 'node.name' on nodes, and some of them are used as a path part
- * in one path on the 'node.pth' property. Need to do an audit of all of this and make it
- * consistent, and create a class called NodePathName to hold the path ones.
- */
 public class NodeName {
 	public static final String HOME = "home";
 	public static final String INBOX = "inbox";
