da19f81f13165d3c4141d91597aa0c3c5e899b5a
Clay Ferguson <wclayf@gmail.com>
2022-08-02 13:23:34 -0500

    auto-logging script context vars

--- src/main/resources/public/ts/plugins/CalcTypeHandler.ts
+++ src/main/resources/public/ts/plugins/CalcTypeHandler.ts
@@ -36,31 +36,48 @@ export class CalcTypeHandler extends TypeBase {
             return new Div("Only the Owner of a Calculation node can run the calculation.");
         }
 
-        // scripts are recommended to put all variables in 'window.qc' to be safest
-        win.qc = {
+        // scripts are recommended to put all variables in 'window.q' to be safest
+        win.q = {
             logs: [],
             log: (v: any) => {
-                win.qc.logs.push(v);
+                win.q.logs.push(v);
             },
             formatCurrency: S.util.formatCurrency
         };
 
         try {
-            win.eval(node.content);
-            return new Pre(win.qc.logs.join("\n"), { className: "calcOutputArea" });
+            let script = node.content;
+            script = this.addFinalScript(script);
+            win.eval(script);
+            return new Pre(win.q.logs.join("\n"), { className: "calcOutputArea" });
         }
         catch (e) {
             return new Pre(e.message + "\n" + e.stack, { className: "calcOutputArea" });
         }
         finally {
-            win.qc = null;
+            win.q = null;
         }
+    }
+
+    // Prints the values of all properts that were assigned to 'qc' during the running of the script, so that
+    // instead of logging a value by calling q.log, you can just make sure the variable is one like 'q.myVar' and
+    // it's final value will always be printed.
+    addFinalScript = (script: string) => {
+        return script + `\n
+            for (let prop in q) {
+                if (q.hasOwnProperty(prop)) {
+                    if (prop!="log" && prop!="logs" && prop!="formatCurrency") {
+                        q.log(prop+": "+q[prop]);
+                    }
+                }
+            }
+        `;
     }
 
     ensureDefaultProperties(node: J.NodeInfo) {
         // this has the unintended behavior of when the user tries to clear out the text it comes back in realtime.
         // if (!node.content) {
-        //     node.content = "qc.a=23;\nqc.b=19;\n\nqc.log('Answer=' + (qc.a + qc.b));";
+        //     node.content = "qc.a=23;\nqc.b=19;\n\nq.log('Answer=' + (qc.a + qc.b));";
         // }
     }
 }
