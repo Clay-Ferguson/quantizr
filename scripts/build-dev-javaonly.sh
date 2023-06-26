57b140e4b2c91cb68ea0a230b8b0a9f85b08825b
Clay Ferguson <wclayf@gmail.com>
2023-06-26 18:21:46 -0500

    script update

--- scripts/build-dev-javaonly.sh
+++ scripts/build-dev-javaonly.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# WARNING: Docker Swarm has issues with this. Sometimes it works 
+# and sometimes it fails.
+
 # This is the script to run if you have Quanta already deployed locally and have only chagned Java source
 # and want to restart the Quanta web app container. Note that this only works because our DEV environment
 # has the following volumes in the docker compose yaml:
@@ -24,20 +27,20 @@ cd ${PRJROOT}
 mvn -T 1C package -DskipTests=true -P${mvn_profile}
 verifySuccess "Maven Build"
 
-dockerDownQuanta
+dockerDown
 # IMPORTANT
 # This sleep is extremely imortant. You can see if you run 'docker ps' that even after scaling down to zero the
 # replica is still 'running' so we have to wait here untl even 'dockerk ps' can confirm Quanta is indeed down, and
 # for now I'm just doing a long enough wait, but depending on computer CPU load this sleep might not even be
 # long enough
-echo "Sleeping 20s for Docker Swarm stabilize" 
-sleep 20s
+echo "Sleeping 30s for Docker Swarm stabilize" 
+sleep 30s
 sudo rm -rf ${QUANTA_BASE}/log/*
 echo "Verifying Stopped"
 # docker container ls --filter name=quanta-stack-dev_quanta-dev*
 docker ps --filter name=quanta-stack-dev_quanta-dev*
 echo "After 'Verifying Stopped' above, nothing should be showing"
-dockerUpQuanta
+dockerUp
 
 serviceCheck ${docker_stack}_quanta-dev
 
