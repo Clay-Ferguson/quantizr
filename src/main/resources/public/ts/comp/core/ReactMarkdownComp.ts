5ab4cbe83244b7087a5e50fbc90d7535226c5012
Clay Ferguson <wclayf@gmail.com>
2023-08-30 02:17:54 -0500

    Adding LaTe support to Markdown

--- src/main/resources/public/ts/comp/core/ReactMarkdownComp.ts
+++ src/main/resources/public/ts/comp/core/ReactMarkdownComp.ts
@@ -1,9 +1,16 @@
 import { createElement, forwardRef } from "react";
 import ReactMarkdown from "react-markdown";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // eslint-disable-next-line react/display-name
 const ReactMarkdownComp = forwardRef((props, ref) => {
-    return createElement(ReactMarkdown as any, { ...props, ref });
+    return createElement(ReactMarkdown as any, {
+        ...props,
+        ref,
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex]
+    });
 });
 
 export default ReactMarkdownComp;
