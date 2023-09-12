7fdb1c30bb4ddfc5e00ded29a397a7b2cc2bbdb4
Clay Ferguson <wclayf@gmail.com>
2023-09-11 20:25:01 -0500

    old comments removed

--- src/main/resources/public/webpack.prod.js
+++ src/main/resources/public/webpack.prod.js
@@ -12,8 +12,6 @@ module.exports = merge(common.config, {
     },
     plugins: [
         new webpack.DefinePlugin({
-            // this was a test: Didn't work. I tried it in the index.html and it was not translated.
-            // Ended up accomplishing this using my 'cachebuster' option on HtmlWebpackPlugin instead.
             // WARNING: The 'stringify' here looks redundant but it's actually requird here by DefinePlugin
             BUILDTIME: JSON.stringify(common.formatDate(new Date())),
             PROFILE: JSON.stringify("prod")
