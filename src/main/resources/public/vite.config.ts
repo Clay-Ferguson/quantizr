da61ec7df67edf6329349ec349a30423bd943c5d
Clay Ferguson <wclayf@gmail.com>
2023-09-12 23:05:14 -0500

    circular ref check added to vite

--- src/main/resources/public/vite.config.ts
+++ src/main/resources/public/vite.config.ts
@@ -1,18 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import circularDependency from "vite-plugin-circular-dependency";
 
-const path = require("path");
+// This will show up as an error, in VSCode which is fine because VSCode is seeing the
+// non-Node version of the tsconfig which is fine and can't really be fixed.
+import path from "path";
+
+declare const __dirname;
 
 export default defineConfig({
     define: {
         BUILDTIME: JSON.stringify(new Date().toLocaleString())
     },
     build: {
-        chunkSizeWarningLimit: 100,
+        chunkSizeWarningLimit: 3000,
         minify: true,
         // sourcemap: true
     },
-    plugins: [react()],
+    plugins: [
+        circularDependency({
+            exclude: "/node_modules/"
+        }),
+        react()
+    ],
     resolve: {
         alias: {
             "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
