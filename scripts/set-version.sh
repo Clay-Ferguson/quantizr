31b5df42567714bac6caad3d03cf9c9ca6ae2ebc
Clay Ferguson <wclayf@gmail.com>
2023-10-17 18:07:35 -0500

    version bump

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # *** WARNING *** This value is hardcoded in a AppProp.java class also (for now). Change it there too.
-export QUANTA_VER="3.0.4"
+export QUANTA_VER="3.0.5"
 echo "Set Version: ${QUANTA_VER}"
