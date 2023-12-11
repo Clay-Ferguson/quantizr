6876391a234992863efc62dd3838a9bae7e42c2b
Clay Ferguson <wclayf@gmail.com>
2023-12-10 20:23:15 -0600

    version bump

--- distro/set-version.sh
+++ distro/set-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # *** WARNING *** This value is hardcoded in a AppProp.java class also (for now). Change it there too.
-export QUANTA_VER="3.0.5"
+export QUANTA_VER="3.0.6"
 echo "Set Version: ${QUANTA_VER}"
