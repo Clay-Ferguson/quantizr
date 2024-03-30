5f4e7094a8582f862e65f95dff232a6b063585b7
Clay Ferguson <wclayf@gmail.com>
2024-03-30 11:34:10 -0500

    build.sh tweaks

--- src/main/resources/public/build.sh
+++ src/main/resources/public/build.sh
@@ -1,12 +1,28 @@
 #!/bin/bash -i
 
-# WARNING: This script is normally called FROM ./scripts/build.sh where some environment setup
+# TIP: If you're running VSCode, and you have issues with the node version inside VSCode terminals not matching
+# the one in your system, you can solve this by running the following command *outside* of VSCode:
+#
+#     nvm alias default <version>
+#
+# Ths works because VSCode will use the 'default' alias and therefore it will use the version you set. After you run
+# the command, restart VSCode, and close/reopen all terminals and you should be good to go.
+
+
+# WARNING: This script is called FROM ./scripts/build.sh where some environment setup
 # and other important precitions MUST be met before running this script
 
 if [ -z "$quanta_domain" ]
 then
     read -p "\$quanta_domain is empty. Don't run this batch file directly. It's run from a 'build-*.sh' file"
     exit
+fi
+
+if [ ! -d "node_modules" ]; then
+    echo "node_modules directory not found, running 'yarn install'..."
+    yarn install
+else
+    echo "node_modules found."
 fi
 
 yarn run ${SASS_SCRIPT}
