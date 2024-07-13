9ee85b59f9d31b70cfafe0e5ec3a6012e8ca6ee1
Clay Ferguson <wclayf@gmail.com>
2024-07-13 18:53:33 -0500

    updating distro folder

--- distro/QuantaAI/quanta-ai.py
+++ distro/QuantaAI/quanta-ai.py
@@ -1,13 +1,31 @@
+import os
 from fastapi import FastAPI, Header
 from pydantic import BaseModel
+from openai import OpenAI
 from langchain.schema import HumanMessage, AIMessage, SystemMessage
 from langchain.chat_models.base import BaseChatModel
 from langchain_anthropic import ChatAnthropic
 from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatPerplexity
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+from langchain_community.utilities.dalle_image_generator import DallEAPIWrapper
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import OpenAI
 from pydantic.v1.types import SecretStr
 from pydantic import BaseModel
 from typing import List, Optional
 import traceback
+
+ANTH_OPUS_MODEL_COMPLETION_CHAT = "claude-3-opus-20240229"
+ANTH_SONNET_MODEL_COMPLETION_CHAT = "claude-3-5-sonnet-20240620"
+OPENAI_MODEL_COMPLETION = "gpt-4o"
+PPLX_MODEL_COMPLETION_ONLINE = "llama-3-sonar-large-32k-online" 
+PPLX_MODEL_COMPLETION_LLAMA3 = "llama-3-70b-instruct"
+PPLX_MODEL_COMPLETION_CHAT = "llama-3-sonar-large-32k-chat"
+
+# Langchain doesn't work. So we call OpenAI directly, for now.
+LANGCHAIN_IMAGE_GEN = False
 
 app = FastAPI()
 
@@ -16,10 +34,10 @@ class AIBaseMessage(BaseModel):
     content: str
 
 class AIResponse(BaseModel):
-    inputTokens: int
-    outputTokens: int
-    content: str
-
+    content: Optional[str]
+    cost: Optional[float]
+    error: Optional[str]
+    
 class AIRequest(BaseModel):
     systemPrompt: Optional[str]
     prompt: str
@@ -28,6 +46,7 @@ class AIRequest(BaseModel):
     model: str
     temperature: float
     maxTokens: int
+    credit: float
 
 @app.get("/")
 def index() -> str:
@@ -38,38 +57,49 @@ def api_query(req: AIRequest,
               api_key: Optional[str] = Header(None, alias="X-api-key")
     ) -> AIResponse:
     try:
-        llm: BaseChatModel = None;
-        if req.service == "anthropic":
-            llm = ChatAnthropic(
-                    model_name=req.model,
-                    temperature=req.temperature,
-                    max_tokens_to_sample=req.maxTokens,
-                    timeout=120,  # timeout in seconds
-                    api_key=SecretStr(api_key),
-                )
-        elif req.service == "openai":
-            llm = ChatOpenAI(
-                model=req.model,
-                temperature=req.temperature,
-                api_key=api_key,
-                verbose=True,
-            )
-        else:
-            raise ValueError(f"Unsupported service: {req.service}")
-
-        # build messages context for LangChain
-        messages = []
-        for msg in req.messages:
-            if msg.type == "human":
-                messages.append(HumanMessage(content=msg.content))
-            elif msg.type == "ai":
-                messages.append(AIMessage(content=msg.content))
-            else:
-                raise ValueError(f"Unsupported message type: {msg.type}")
+        # for now we'll max out at 100k tokens allowed
+        if (req.maxTokens > 100000): 
+            req.maxTokens = 100000
             
-        # Add the current human question to the messages
-        messages.append(HumanMessage(content=req.prompt))
+        llm = getChatModel(req, api_key)
+        messages = buildMessages(req)
+        
+        # Estimate input tokens
+        input_text = "".join([msg.content for msg in req.messages])
+        input_tokens = int((len(input_text)+3) / 3)
+                
+        # calculate predicted cost
+        cost: float = calculate_cost(input_tokens, req.maxTokens, req.model)
+        if (cost > req.credit):
+            return AIResponse(content=None, cost=None, error="Insufficient credit. Add funds to your account.")         
 
+        response = llm.invoke(list(messages))
+
+        # Estimate output tokens
+        output_tokens = int((len(response.content)+3) / 3)        
+        cost = calculate_cost(input_tokens, output_tokens, req.model)
+
+        # not make our return value and return it
+        ret = AIResponse(content=response.content, cost=cost, error=None)
+        return ret
+    except Exception as e:
+        return AIResponse(content=None, cost=None, error=str(e)+"\n"+traceback.format_exc())
+
+def buildMessages(req):
+    messages = []
+    for msg in req.messages:
+        if msg.type == "human":
+            messages.append(HumanMessage(content=msg.content))
+        elif msg.type == "ai":
+            messages.append(AIMessage(content=msg.content))
+        else:
+            raise ValueError(f"Unsupported message type: {msg.type}")
+        
+    # Add the current human question to the messages
+    messages.append(HumanMessage(content=req.prompt))
+
+    # todo-0: we can do this cleaner, but we're doing the "If perplexity online model don't set system prompt" here
+    if "online" not in req.model:    
         if req.systemPrompt is None or req.systemPrompt == "":
             req.systemPrompt = "You are a helpful agent."
         
@@ -78,18 +108,92 @@ def api_query(req: AIRequest,
             messages.insert(0, SystemMessage(content=req.systemPrompt))
         # else we set the first message to the system prompt
         else:
-            messages[0] = SystemMessage(content=req.systemPrompt)
+            messages[0] = SystemMessage(content=req.systemPrompt)        
 
-        response = llm.invoke(list(messages))
+    return messages
 
-        # Estimate input tokens
-        input_text = "".join([msg.content for msg in req.messages])
-        input_tokens = int((len(input_text) +3) / 3)
+def getChatModel(req: AIRequest, api_key) -> BaseChatModel:
+    llm: BaseChatModel = None;
+    if req.service == "anthropic":
+        llm = ChatAnthropic(
+                model_name=req.model,
+                temperature=req.temperature,
+                max_tokens_to_sample=req.maxTokens,
+                timeout=120,  # timeout in seconds
+                api_key=SecretStr(api_key),
+            )
+    elif req.service == "openai":
+        llm = ChatOpenAI(
+            model=req.model,
+            temperature=req.temperature,
+            max_tokens=req.maxTokens,
+            timeout=120,  # timeout in seconds
+            api_key=api_key,
+            verbose=True,
+        )
+    elif req.service == "perplexity":
+        llm = ChatPerplexity(
+            model=req.model,
+            temperature=req.temperature,
+            max_tokens_to_sample=req.maxTokens,
+            request_timeout=120,  # timeout in seconds
+            pplx_api_key=api_key,
+        )
+    else:
+        raise ValueError(f"Unsupported service: {req.service}")
+    return llm
 
-        # Estimate output tokens
-        output_tokens = int((len(response.content)+3) / 3)
+# https://www.anthropic.com/pricing#anthropic-api
+def calculate_cost(input_tokens, output_tokens, model) -> float:
+    input_ppm = 0
+    output_ppm = 0
 
-        # not make our return value and return it
-        return AIResponse(content=response.content, inputTokens=input_tokens, outputTokens=output_tokens)
-    except Exception as e:
-        return AIResponse(content="```\n"+str(e)+"\n"+traceback.format_exc()+"```", inputTokens=0, outputTokens=0)
+    # We detect using startswith, because the actual model used will be slightly different than the
+    # one specified
+    if model == OPENAI_MODEL_COMPLETION:
+        # prices per kilotoken
+        input_ppk = 0.005
+        output_ppk = 0.015
+        return (input_tokens * input_ppk / 1000) + (output_tokens * output_ppk / 1000)
+
+    elif model == ANTH_OPUS_MODEL_COMPLETION_CHAT:
+        # prices per megatoken
+        input_ppm = 15
+        output_ppm = 75
+        return (input_tokens * input_ppm / 1000000) + \
+               (output_tokens * output_ppm / 1000000)
+
+    elif model == ANTH_SONNET_MODEL_COMPLETION_CHAT:
+        # prices per megatoken
+        input_ppm = 3.0
+        output_ppm = 15.0
+        return (input_tokens * input_ppm / 1000000) + \
+               (output_tokens * output_ppm / 1000000)
+
+    # 70B model
+    elif model == PPLX_MODEL_COMPLETION_CHAT:
+        # prices per megatoken
+        input_ppm = 1.0
+        output_ppm = 1.0
+        return (input_tokens * input_ppm / 1000000) + \
+               (output_tokens * output_ppm / 1000000)
+
+    # 70B model
+    elif model == PPLX_MODEL_COMPLETION_LLAMA3:
+        # prices per megatoken
+        input_ppm = 1.0
+        output_ppm = 1.0
+        return (input_tokens * input_ppm / 1000000) + \
+               (output_tokens * output_ppm / 1000000)
+
+    # 70B model
+    elif model == PPLX_MODEL_COMPLETION_ONLINE:
+        input_ppm = 1.0
+        output_ppm = 1.0
+        input_price_per_req = 0.005
+        return input_price_per_req + (input_tokens * input_ppm / 1000000) + \
+               (output_tokens * output_ppm / 1000000)
+
+    else:
+        raise RuntimeError(f"Model not supported: {model} is not supported.")
+
