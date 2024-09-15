500b5c24c593642d8b746e8b908efdcc3aab42fc
Clay Ferguson <wclayf@gmail.com>
2024-09-15 16:26:22 -0500

    created a scratchpad class LayoutDemo for testing comonents

---
+++ src/main/resources/public/src/comp/LayoutDemo.ts
@@ -0,0 +1,37 @@
+import { getAs } from "../AppContext";
+import { Div } from "./core/Div";
+import { Progress } from "./core/Progress";
+import { Main } from "./Main";
+
+/* This is just a scratchpad for testing layout components and styles. */
+export class LayoutDemo extends Main {
+
+    constructor() {
+        super(null, { id: "appPanelId", role: "main" });
+    }
+
+    override preRender(): boolean | null {
+        const ast = getAs();
+
+        if (!ast.appInitComplete) {
+            this.children = [new Progress()];
+            return true;
+        }
+
+        const children = [];
+        for (let i = 0; i < 10; i++) {
+            children.push(new Div("Brick"+i, {
+                className: "brick brickWidth100px brickInline",
+            }));
+        }
+
+        this.children = [
+            new Div("Div with inline-block children"),
+            new Div(null, {
+                className: "brickRow",
+                id: "appMainContainer"
+            }, children)
+        ];
+        return true;
+    }
+}
--- src/main/resources/public/src/comp/core/AppContainer.ts
+++ src/main/resources/public/src/comp/core/AppContainer.ts
@@ -1,10 +1,13 @@
 import { createElement } from "react";
 import { AppContext, getAs, initDispatch } from "../../AppContext";
 import { App } from "../App";
+import { LayoutDemo } from "../LayoutDemo";
+
+const layoutDemo = false;
 
 /* Builds the application root element wrapping inside AppContext.Provider for our state management */
 export default function AppContainer() {
     initDispatch();
-    const app = new App();
+    const app = layoutDemo ? new LayoutDemo() : new App();
     return createElement(AppContext.Provider, { value: getAs() }, createElement(app._render, app.attribs));
 }
