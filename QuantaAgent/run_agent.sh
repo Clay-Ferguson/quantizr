6f44f8d102ce362e6b0454e4a0dc3289fae6746b
Clay Ferguson <wclayf@gmail.com>
2025-01-31 17:24:35 -0600

    removed obsolete QuantaAgent project

--- QuantaAgent/run_agent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ "$CONDA_DEFAULT_ENV" == "quanta_agent" ]]; then
-  python3 Quanta_Agent.py
-else
-  echo "Wrong Conda Environment: Expected quanta_agent but found $CONDA_DEFAULT_ENV"
-  sleep 10s
-  exit 1
-fi
