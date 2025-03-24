83f9659aa755046488ec5b0fe6fc533d6674edf0
Clay Ferguson <wclayf@gmail.com>
2025-03-24 15:22:53 -0500

    removing AppContext.Provider (deprecated in React 19)

--- src/main/resources/public/src/comp/core/AppContainer.ts
+++ src/main/resources/public/src/comp/core/AppContainer.ts
@@ -5,9 +5,10 @@ import { LayoutDemo } from "../LayoutDemo";
 
 const layoutDemo = false;
 
-/* Builds the application root element wrapping inside AppContext.Provider for our state management */
+/* Builds the application root element wrapping inside AppContext for our state management */
 export default function AppContainer() {
     initDispatch();
     const app = layoutDemo ? new LayoutDemo() : new App();
-    return createElement(AppContext.Provider, { value: getAs() }, createElement(app._render, app.attribs));
+    // Historical note: This `AppContext` used to be `AppContext.Provider` but as of React 19, we no longer need Provider.
+    return createElement(AppContext, { value: getAs() }, createElement(app._render, app.attribs));
 }
